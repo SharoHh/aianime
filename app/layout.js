e5d016218ec80ee5cd2bb681cf458c4434b6703e
SharoHh <148870610+SharoHh@users.noreply.github.com>
2026-05-24 22:18:55 +0700

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return <html lang="ru">
     <head>
+      <meta name="theme-color" content="#fdfcff" />
+      <style dangerouslySetInnerHTML={{__html:'html{background:#fdfcff;color-scheme:light;}body{margin:0;background:#fdfcff;}body::selection{background:#eadcff;color:#15122d;}'}} />
       <link rel="preconnect" href="https://kodikplayer.com" crossOrigin="anonymous" />
       <link rel="dns-prefetch" href="https://kodikplayer.com" />
       <link rel="preconnect" href="https://i.kodikres.com" crossOrigin="anonymous" />
